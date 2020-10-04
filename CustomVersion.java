01b9de56558f50b48d631cdd9458357b272d0f0e
4233242 <72352848+4233242@users.noreply.github.com>
2020-10-04 18:01:48 +0100

    Add files via upload

---
+++ CustomVersion.java
@@ -0,0 +1,36 @@
+package net.minehorizon.customversionvelocity;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyPingEvent;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.server.ServerPing;
+import org.slf4j.Logger;
+
+@Plugin(id = "CustomVersion", name = "VelocityCustomVersion", version = "1.0-SNAPSHOT", description = "Show the supported versions", authors = {"Aderm"})
+public class CustomVersion {
+
+    private final ProxyServer server;
+    private final Logger logger;
+
+    @Inject
+    public CustomVersion(ProxyServer server, Logger logger) {
+        this.server = server;
+        this.logger = logger;
+
+        logger.info("CustomVersion loaded");
+    }
+
+    public void pingEvent(ProxyPingEvent event) {
+        event.setPing(ServerPing.builder().getVersion());
+    }
+
+    public ProxyServer getServer() {
+        return server;
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+
+}
